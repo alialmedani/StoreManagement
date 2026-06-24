6b958de66223f372bebf2e7e18149e5734b31409
alialmedani <alimedani02@gmail.com>
2026-06-24 13:08:37 +0300

    Add CustomerAddress column to AppOrders table in migration

---
+++ src/StoreManagement.EntityFrameworkCore/Migrations/20260624095629_AddOrderCustomerAddress.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace StoreManagement.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddOrderCustomerAddress : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "CustomerAddress",
+                table: "AppOrders",
+                type: "nvarchar(500)",
+                maxLength: 500,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CustomerAddress",
+                table: "AppOrders");
+        }
+    }
+}
