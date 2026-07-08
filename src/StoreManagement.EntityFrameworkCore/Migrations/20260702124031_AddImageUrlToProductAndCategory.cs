672a503552d251d77999c11e67ca16dad775d61a
alialmedani <alimedani02@gmail.com>
2026-07-08 14:53:43 +0300

    Add ImageUrl property to Product and Category tables in migration

---
+++ src/StoreManagement.EntityFrameworkCore/Migrations/20260702124031_AddImageUrlToProductAndCategory.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace StoreManagement.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddImageUrlToProductAndCategory : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ImageUrl",
+                table: "AppProducts",
+                type: "nvarchar(500)",
+                maxLength: 500,
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ImageUrl",
+                table: "AppCategories",
+                type: "nvarchar(500)",
+                maxLength: 500,
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ImageUrl",
+                table: "AppProducts");
+
+            migrationBuilder.DropColumn(
+                name: "ImageUrl",
+                table: "AppCategories");
+        }
+    }
+}
